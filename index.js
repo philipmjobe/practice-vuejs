0252683fa5c209e381698b6b58ab63fbe08030c1
Philip <philipmjobe@gmail.com>
2021-11-04 12:58:38 -0400

    added all of the html and javascript to get to the end of the first exercise

---
+++ index.js
@@ -0,0 +1,50 @@
+/* global Vue */
+
+var app = new Vue({
+  el: "#app",
+  data: function () {
+    return {
+      message: "Hello from JavaScript!",
+    };
+  },
+});
+
+var app2 = new Vue({
+  el: "#app-2",
+  data: {
+    message: "You loaded this page on " + new Date().toLocaleString(),
+  },
+});
+
+var app3 = new Vue({
+  el: "#app-3",
+  data: {
+    seen: true,
+  },
+});
+
+var app4 = new Vue({
+  el: "#app-4",
+  data: {
+    todos: [{ text: "Learn JavaScript" }, { text: "Learn Vue" }, { text: "Build something awesome" }],
+  },
+});
+
+var app5 = new Vue({
+  el: "#app-5",
+  data: {
+    message: "Hello Vue.js!",
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split("").reverse().join("");
+    },
+  },
+});
+
+var app6 = new Vue({
+  el: "#app-6",
+  data: {
+    message: "Hello Vue!",
+  },
+});
