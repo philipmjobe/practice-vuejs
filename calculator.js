7302b04d19064fe0980d5329da3597f392253d2e
Philip <philipmjobe@gmail.com>
2021-11-04 14:34:26 -0400

    added logic to calculator that i neeed to fix

---
+++ calculator.js
@@ -0,0 +1,19 @@
+/*global Vue*/
+
+var app = new Vue({
+  el: "#app",
+  data: function () {
+    return {
+      message: "Hello Vue!",
+      number1: 0,
+      number2: 0,
+      number3: 0,
+      sum: 0,
+    };
+  },
+  methods: {
+    calculateSum: function () {
+      this.sum = parseInt(this.number1) + parseInt(this.number2) + parseInt(this.number3);
+    },
+  },
+});
